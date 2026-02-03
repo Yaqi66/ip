89b25c14d8dca8bc34eba5a972babd56c5b01a82
Yaqi66 <yangy10@tcd.ie>
2026-02-04 06:46:30 +0800

    Level 3. Add the ability to mark task as done not done

--- src/main/java/Pudding.java
+++ src/main/java/Pudding.java
@@ -16,16 +16,35 @@ public class Pudding {
         System.out.println(line);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        ArrayList list = new ArrayList();
+        ArrayList<Task> list = new ArrayList<>();
         while(!input.equals("bye")) {
             if(input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i+1) + ". " +list.get(i));
+                    System.out.println((i+1) + ".[" +list.get(i).getStatusIcon()+"] "+list.get(i).description);
                 }
             }
             else {
-                System.out.println("added: "+input);
-                list.add(input);
+                if (input.startsWith("mark")) {
+                    int index = Integer.parseInt(input.substring(5));
+                    if(0<index && index<=list.size()) {
+                        System.out.println("Nice! I've marked this task as done:");
+                        list.get(index - 1).isDone = true;
+                        System.out.println((" [" +list.get(index-1).getStatusIcon()+"] "+list.get(index-1).description));
+                    }
+                }
+                else if (input.startsWith("unmark")) {
+                    int index = Integer.parseInt(input.substring(7));
+                    if(0<index && index<=list.size()) {
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        list.get(index - 1).isDone = false;
+                        System.out.println((" [" +list.get(index-1).getStatusIcon()+"] "+list.get(index-1).description));
+                    }
+                }
+                else{
+                    System.out.println("added: "+input);
+                    list.add(new Task(input));
+                }
             }
             System.out.println(line);
             input = sc.nextLine();
@@ -33,4 +52,20 @@ public class Pudding {
         System.out.println("\nBye. Hope to see you again soon!\n");
         System.out.println(line);
     }
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+    }
+
 }
