4621f613ec214dc52cb229f71f898f2737c525ce
Yaqi66 <yangy10@tcd.ie>
2026-02-04 03:23:51 +0800

    Level-0: Rename the chatbot

---
+++ src/main/java/Pudding.java
@@ -0,0 +1,18 @@
+public class Pudding {
+    public static void main(String[] args) {
+        String logo = """                                               
+                                                    
+        █████▄ ██  ██ ████▄  ████▄  ██ ███  ██  ▄████  
+        ██▄▄█▀ ██  ██ ██  ██ ██  ██ ██ ██ ▀▄██ ██  ▄▄▄ 
+        ██     ▀████▀ ████▀  ████▀  ██ ██   ██  ▀███▀  
+                                                    """;
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Pudding");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("\nBye. Hope to see you again soon!\n");
+        System.out.println(line);
+    }
+}
