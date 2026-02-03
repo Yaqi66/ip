fca5d920cd61f03637a4b994d0ee052a0ff7925b
Yaqi66 <yangy10@tcd.ie>
2026-02-04 03:39:42 +0800

    Level 2: Enable chatbox to echo commands entered by the user

--- src/main/java/Pudding.java
+++ src/main/java/Pudding.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Pudding {
     public static void main(String[] args) {
         String logo = """                                               
@@ -12,6 +13,13 @@ public class Pudding {
         System.out.println("Hello! I'm Pudding");
         System.out.println("What can I do for you?");
         System.out.println(line);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            System.out.println(line);
+            input = sc.nextLine();
+        }
         System.out.println("\nBye. Hope to see you again soon!\n");
         System.out.println(line);
     }
