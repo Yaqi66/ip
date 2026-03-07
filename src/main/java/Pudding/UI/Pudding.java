1c9bcc6669537dbb32bec3491a5a26fa8dccb050
Yaqi66 <yangy10@tcd.ie>
2026-03-07 16:34:13 +0800

    no message

--- src/main/java/Pudding/UI/Pudding.java
+++ src/main/java/Pudding/UI/Pudding.java
@@ -1,6 +1,14 @@
 ﻿package Pudding.UI;
 
-/**
+/**An automated smoke testing of your iP JAR file on Linux, Mac, and Windows found the application crashes with the following error(s) upon launching:
+
+ On Linux, using Java 17, for the pudding.jar uploaded on 03-05-2026 (MM-DD-YYYY) at 04:00:03:
+ Error: LinkageError occurred while loading main class pudding.ui.Pudding java.lang.UnsupportedClassVersionError: pudding/ui/Pudding has been compiled by a more recent version of the Java Runtime (class file version 65.0), this version of the Java Runtime only recognizes class file versions up to 61.0
+ On Linux(WSL), using Java 17, for the pudding.jar uploaded on 03-05-2026 (MM-DD-YYYY) at 04:00:03:
+ Error: LinkageError occurred while loading main class pudding.ui.Pudding java.lang.UnsupportedClassVersionError: pudding/ui/Pudding has been compiled by a more recent version of the Java Runtime (class file version 65.0), this version of the Java Runtime only recognizes class file versions up to 61.0
+ On Mac(ARM), using Java 17, for the pudding.jar uploaded on 03-05-2026 (MM-DD-YYYY) at 04:00:03:
+ Error: LinkageError occurred while loading main class pudding.ui.Pudding java.lang.UnsupportedClassVersionError: pudding/ui/Pudding has been compiled by a more recent version of the Java Runtime (class file version 65.0), this version of the Java Runtime only recognizes class file versions up to 61.0
+ FYI, we smoke-tested the JAR on following environments: Linux, Linux(WSL), Mac(ARM).
  * Main class for the Pudding chatbot application.
  * Owns the top-level {@link Storage}, {@link TaskList}, and {@link Ui} instances
  * and drives the main command loop.
